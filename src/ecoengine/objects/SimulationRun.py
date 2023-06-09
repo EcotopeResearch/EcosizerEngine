running kGperkWh calculation but some stuff might be wrong

src/ecoengine/objects/SimulationRun.py
@@ -1,13 +1,15 @@
 from .Building import Building
 import numpy as np
 from ecoengine.constants.Constants import *
-from .systemConfigUtils import hrToMinList, roundList
+from .systemConfigUtils import hrToMinList, roundList, hrTo15MinList
 from plotly.graph_objs import Figure, Scatter
 from plotly.offline import plot
 from plotly.subplots import make_subplots
+import os
+import csv
 
 class SimulationRun:
-    def __init__(self, hwGenRate, hwDemand, V0, Vtrig, pV, pGen, pheating, mixedStorT_F, building : Building, loadShiftSchedule, doLoadshift = False):
+    def __init__(self, hwGenRate, hwDemand, V0, Vtrig, pV, pGen, pRun, pheating, mixedStorT_F, building : Building, loadShiftSchedule, minuteIntervals, doLoadshift = False):
         """
         Initializes arrays needed for 3-day simulation
 
@@ -36,15 +38,97 @@ class SimulationRun:
         self.pV = pV
         self.pheating = pheating
         self.pGen = pGen
+        self.pRun = pRun # amount of time in interval primary tank is heating
         self.mixedStorT_F = mixedStorT_F
         self.building = building
+        self.minuteIntervals = minuteIntervals
         self.doLoadShift = doLoadshift
         self.loadShiftSchedule = loadShiftSchedule
+        self.monthlyCityWaterT_F = None
+        self.oat = [] # oat by hour
+        self.cap = [] # capacity at every time interval
+        self.kGperkWh = [] # the kG CO2 per kWh at every time interval
 
     def getIncomingWaterT(self, i):
-        return self.building.incomingT_F
+        if self.monthlyCityWaterT_F is None:
+            return self.building.incomingT_F # default city water temp
+        else:
+            dayOfYear = (i // (60/self.minuteIntervals)) // 24
+            if dayOfYear < 31:
+                # jan
+                return self.monthlyCityWaterT_F[0]
+            elif dayOfYear < 59:
+                # feb
+                return self.monthlyCityWaterT_F[1]
+            elif dayOfYear < 90:
+                # mar
+                return self.monthlyCityWaterT_F[2]
+            elif dayOfYear < 120:
+                # apr
+                return self.monthlyCityWaterT_F[3]
+            elif dayOfYear < 151:
+                # may
+                return self.monthlyCityWaterT_F[4]
+            elif dayOfYear < 181:
+                # jun
+                return self.monthlyCityWaterT_F[5]
+            elif dayOfYear < 212:
+                # jul
+                return self.monthlyCityWaterT_F[6]
+            elif dayOfYear < 243:
+                # aug
+                return self.monthlyCityWaterT_F[7]
+            elif dayOfYear < 273:
+                # sep
+                return self.monthlyCityWaterT_F[8]
+            elif dayOfYear < 304:
+                # oct
+                return self.monthlyCityWaterT_F[9]
+            elif dayOfYear < 334:
+                # nov
+                return self.monthlyCityWaterT_F[10]
+            elif dayOfYear < 365:
+                # dec
+                return self.monthlyCityWaterT_F[11]
+            else:
+                raise Exception("Cold water temperature data not available past one year.")
+            
+    def getAvgIncomingWaterT(self):
+        if self.monthlyCityWaterT_F is None:
+            return self.building.incomingT_F # default city water temp
+        else:
+            return ((self.monthlyCityWaterT_F[0]*31) + (self.monthlyCityWaterT_F[1]*28) + (self.monthlyCityWaterT_F[2]*31) + (self.monthlyCityWaterT_F[3]*30) \
+                + (self.monthlyCityWaterT_F[4]*31) + (self.monthlyCityWaterT_F[5]*30) + (self.monthlyCityWaterT_F[6]*31) + (self.monthlyCityWaterT_F[7]*31) \
+                + (self.monthlyCityWaterT_F[8]*30) + (self.monthlyCityWaterT_F[9]*31) + (self.monthlyCityWaterT_F[10]*30) + (self.monthlyCityWaterT_F[11]*31)) / 365
+            # return ((self.monthlyCityWaterT_F[0]) + (self.monthlyCityWaterT_F[1]) + (self.monthlyCityWaterT_F[2]) + (self.monthlyCityWaterT_F[3]) \
+            #     + (self.monthlyCityWaterT_F[4]) + (self.monthlyCityWaterT_F[5]) + (self.monthlyCityWaterT_F[6]) + (self.monthlyCityWaterT_F[7]) \
+            #     + (self.monthlyCityWaterT_F[8]) + (self.monthlyCityWaterT_F[9]) + (self.monthlyCityWaterT_F[10]) + (self.monthlyCityWaterT_F[11])) / 12
+            
+    
+    def setMonthlyCityWaterT_F(self, monthlyCityWaterT_F):
+        if len(monthlyCityWaterT_F) != 12:
+            raise Exception("Monthly city water temperature data must have 12 entries (one for every month).")
+        for i in range(12):
+            if not isinstance(monthlyCityWaterT_F[i], float):
+                raise Exception(str(monthlyCityWaterT_F[i]) + " is an invalid city water tempurature for month "+str(i+1)+".")
+        self.monthlyCityWaterT_F = monthlyCityWaterT_F
 
-    def returnSimResult(self):
+    def addOat(self, oat_value):
+        if not (isinstance(oat_value, float) or isinstance(oat_value, int)):
+            raise Exception(str(oat_value) + " is an invalid outdoor air tempurature.")
+        self.oat.append(oat_value)
+    
+    def addCap(self, cap_value):
+        if not (isinstance(cap_value, float) or isinstance(cap_value, int)):
+            raise Exception(str(cap_value) + " is an invalid system capacity.")
+        self.cap.append(cap_value)
+
+    def addKGperkWh(self, kGperkWh_value):
+        if not (isinstance(kGperkWh_value, float) or isinstance(kGperkWh_value, int)):
+            raise Exception(str(kGperkWh_value) + " is an invalid system kGperkWh value.")
+        self.kGperkWh.append(kGperkWh_value)
+
+    def returnSimResult(self, kWhCalc = False, climateZone = 11):
         retList = [roundList(self.pV, 3),
             roundList(self.hwGenRate * self.loadShiftSchedule, 3),
             roundList(self.hwDemand, 3),
@@ -57,7 +141,19 @@ class SimulationRun:
         if hasattr(self, 'hw_outSwing'):
             retList.append(self.hw_outSwing)
 
+        #TODO this is a temp solution. should not stick around. Only doing for 15 min intervals
+        if kWhCalc and len(self.oat) == 8760:
+            if self.minuteIntervals == 15:
+                retList.append(self.pRun)
+                retList.append(hrTo15MinList(self.oat))
+                retList.append(self.cap)
+                retList.append(self.kGperkWh)
+                retList.append(sum(self.kGperkWh))
+                retList.append(self.getAvgIncomingWaterT())               
         return retList
+    
+    # def getKgCO2perkWh(self):
+
     
     def plotStorageLoadSim(self, return_as_div=True):
         """
@@ -74,6 +170,7 @@ class SimulationRun:
         div/fig
             plot_div
         """
+        # TODO make this function work for not 1 minute intervals
         hrind_fromback = 24 # Look at the last 24 hours of the simulation not the whole thing
 
         run = np.array(roundList(self.pGen,3)[-(60*hrind_fromback):])*60

src/ecoengine/objects/SystemConfig.py
@@ -173,7 +173,8 @@ class SystemConfig:
 
         pheating = False
 
-        pGen = [0] * (len(hwDemand))
+        pGen = [0] * len(hwDemand)
+        pRun = [0] * len(hwDemand)
 
         print(len(hwDemand))
         print(len(Vtrig))
@@ -182,12 +183,13 @@ class SystemConfig:
         if initPV:
             pV[0] = initPV
 
-        return SimulationRun(hwGenRate, hwDemand, V0, Vtrig, pV, pGen, pheating, mixedStorT_F, building, loadshiftSched, self.doLoadShift)
+        return SimulationRun(hwGenRate, hwDemand, V0, Vtrig, pV, pGen, pRun, pheating, mixedStorT_F, building, loadshiftSched, minuteIntervals, self.doLoadShift)
     
     def runOneSystemStep(self, simRun : SimulationRun, i, minuteIntervals = 1):
         mixedDHW = mixVolume(simRun.hwDemand[i], simRun.mixedStorT_F, simRun.getIncomingWaterT(i), simRun.building.supplyT_F) 
         mixedGHW = mixVolume(simRun.hwGenRate, simRun.mixedStorT_F, simRun.getIncomingWaterT(i), simRun.building.supplyT_F)
-        simRun.pheating, simRun.pV[i], simRun.pGen[i] = self.runOnePrimaryStep(simRun.pheating, simRun.V0, simRun.Vtrig[i], simRun.pV[i-1], mixedDHW, mixedGHW, simRun.Vtrig[i-1]) 
+        simRun.pheating, simRun.pV[i], simRun.pGen[i], simRun.pRun[i] = self.runOnePrimaryStep(simRun.pheating, simRun.V0, simRun.Vtrig[i], simRun.pV[i-1], mixedDHW, mixedGHW, simRun.Vtrig[i-1],
+                                                                                               minuteIntervals = minuteIntervals) 
     
     def _setLoadShift(self, loadShiftSchedule, loadUpHours, aquaFract, aquaFractLoadUp, aquaFractShed, storageT_F, loadUpT_F, loadShiftPercent=1):
         """
@@ -580,7 +582,7 @@ class SystemConfig:
         return [volN, capN, N]
 
     
-    def runOnePrimaryStep(self, pheating, V0, Vtrig, Vcurr, hw_out, hw_in, Vtrig_previous):
+    def runOnePrimaryStep(self, pheating, V0, Vtrig, Vcurr, hw_out, hw_in, Vtrig_previous, minuteIntervals = 1):
         """
         Runs one step on the primary system. This changes the volume of the primary system
         by assuming there is hot water removed at a volume of hw_out and hot water
@@ -618,29 +620,34 @@ class SystemConfig:
         """
         hw_generated = 0
         Vnew = 0
+        time_ran = 0
         Vtrig_normal = np.ceil(self.PVol_G_atStorageT * (1 - self.aquaFract))
         if pheating:
             Vnew = Vcurr + hw_in - hw_out # If heating, generate HW and lose HW
             hw_generated = hw_in
+            time_ran = 1
 
         else:
             Vnew = Vcurr - hw_out # So lose HW
             if Vnew < Vtrig: # If should heat
                 pheating = True
             if Vnew < Vtrig_previous:
-                time_missed = min((Vtrig_previous - Vnew)/hw_out, 1) # Volume below turn on / rate of draw gives time below tigger (aquastat)
-                Vnew += hw_in * time_missed
-                hw_generated = hw_in * time_missed
+                time_ran = min((Vtrig_previous - Vnew)/hw_out, 1) # Volume below turn on / rate of draw gives time below tigger (aquastat)
+                Vnew += hw_in * time_ran
+                hw_generated = hw_in * time_ran
 
-        if Vtrig == np.ceil(self.PVol_G_atStorageT * (1 - self.aquaFractShed)) and Vnew > Vtrig_normal:
+        if self.doLoadShift and Vtrig == np.ceil(self.PVol_G_atStorageT * (1 - self.aquaFractShed)) and Vnew > Vtrig_normal:
+            # stop heating if hw has met the normal aquastat fraction during a shed period
             time_over = (Vnew - Vtrig_normal) / (hw_in - hw_out) # Volume over generated / rate of generation gives time above full
             Vnew = Vtrig_normal - hw_out * time_over # Make full with missing volume
-            hw_generated = hw_in * (1-time_over)
+            time_ran = 1-time_over
+            hw_generated = hw_in * time_ran
             pheating = False # Stop heating
         elif Vnew > V0: # If overflow
             time_over = (Vnew - V0) / (hw_in - hw_out) # Volume over generated / rate of generation gives time above full
             Vnew = V0 - hw_out * time_over # Make full with missing volume
-            hw_generated = hw_in * (1-time_over)
+            time_ran = 1-time_over
+            hw_generated = hw_in * time_ran
             pheating = False # Stop heating
         elif Vtrig < Vtrig_previous and Vnew > Vtrig: # turn off heating if aquastat changes and we find we are above it.
             pheating = False
@@ -648,7 +655,7 @@ class SystemConfig:
         if Vnew < 0:
            raise Exception("Primary storage ran out of Volume!") 
 
-        return pheating, Vnew, hw_generated
+        return pheating, Vnew, hw_generated, time_ran * minuteIntervals
     
     def _calcPrelimVol(self, loadUpHours, loadshape, building):
         '''