b793569dffe31d6931c1cceab92211f239e874ff
Nolan Price <nolan.price@gmail.com>
2023-09-12 12:44:15 -0700

    fixing little grammer error

--- src/ecoengine/objects/systemConfigUtils.py
+++ src/ecoengine/objects/systemConfigUtils.py
@@ -147,7 +147,7 @@ def createSizingCurvePlot(x, y, startind, loadshifting = False):
     """
     fig = Figure()
     
-    hovertext = 'Storage Volume: %{x:.1f} gallons \nHeating Capacity: %{y:.1f}' if not loadshifting else 'CDF: %{x:.1f} gallons \Storage Volume: %{y:.1f}' 
+    hovertext = 'Storage Volume: %{x:.1f} gallons \nHeating Capacity: %{y:.1f}' if not loadshifting else 'CDF: %{x:.1f} gallons \nStorage Volume: %{y:.1f}' 
 
     fig.add_trace(Scatter(x=x, y=y,
                     visible=True,
